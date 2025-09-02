bb6029f36e80cf0c3d11c75ea4af349f8e8f690c
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-09-02 02:19:04 -0300

    feat: adicionar componentes Projects e ProjectCard com estrutura e dados mapeados

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,27 +1,38 @@
 import type { ProjectPropType } from "../types";
+import { FaGithub } from "react-icons/fa";
 
 export default function ProjectCard({id, title, description, github, demo, img}: ProjectPropType){
     return(
-        <div key={id} className="flex gap-6 items-center max-w-7xl mx-auto px-6 h-96">
-            <div className="w-1/2">
-                <figure className="flex justify-center items-center bg-stone-200 rounded-2xl shadow-lg h-auto">
+        <div key={id} className="flex gap-6 items-center max-w-7xl mx-auto p-6 h-96">
+            <div className="w-1/3 h-full flex justify-start items-center">
+                <figure className="flex justify-center items-center bg-stone-200 rounded-2xl shadow-lg p-6 w-full h-full">
                     <img 
                         src={img} 
                         alt={title} 
-                        className="w-3/4 h-auto rounded-2xl object-contain"    
+                        className="w-4/4 rounded-2xl object-contain"    
                     />
                 </figure>
             </div>
-            <div className="w-1/2">
-                <h3 className="text-2xl font-bold text-gray-900">
+            <div className="w-1/2 flex flex-col justify-start">
+                <h3 className="text-2xl font-bold text-gray-900 border-b border-gray-200">
                     {title}
                 </h3>
                 <p className="text-gray-600 mt-4 mb-4 text-justify h-36 flex items-center">
                     {description}
                 </p>
-                <div className="flex flex-col border-t py-2">
-                    <a href={github}>Ver Projeto</a>
-                    <a href={demo}>Live Demo</a>
+                <div className="flex border-t py-2 gap-4">
+                    <a href={github}
+                        target="_blank"
+                        className="flex bg-gray-800 rounded-full px-6 py-2 hover:bg-gray-700 items-center gap-3 text-white"
+                    >
+                        Ver Projeto <FaGithub/>
+                    </a>
+                    <a href={demo}
+                        target="_blank"
+                        className="bg-gray-800 rounded-full px-6 py-2 hover:bg-gray-700 text-white"
+                    >
+                        Live Demo
+                    </a>
                 </div>
             </div>
         </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,18 +4,18 @@ import ProjectCard from "./ProjectCard";
 export default function Projects(){
 
     return(
-        <section className="w-full py-16 bg-neutral-50">
+        <section className="w-full py-16 bg-neutral-50 border-b border-gray-200">
             <div className="max-w-7xl mx-auto px-6 mb-22">
                 <div className="mb-12">
                     <h2 className="text-neutral-900 text-3xl font-bold">
-                        Projetos em destaque
+                        Alguns dos meus melhores projetos front end.
                     </h2>
                     <p className="mt-2 text-ms text-gray-600">
-                        Aqui estão alguns dos meus melhores projetos front end.
+                        Aqui você encontra alguns projetos que fiz colocando criatividade e código lado a lado. Cada um mostra minha evolução e paixão por criar interfaces que funcionam e encantam.
                     </p>
                 </div>
             </div>
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-14 max-w-7xl mx-auto">
                 {projectsData.map((projetos) => (
                     <ProjectCard
                         id={projetos.id}
