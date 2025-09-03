985a35b3f8689dd196315fd3ea1a7a35f7c06698
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-09-02 21:10:02 -0300

    feat: Adiciona imagem a pasta public/IMG e ao componente Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import Projects from "../components/Projects";
+import MyPhoto from '../../public/IMG/myPhoto.png';
 
 export default function Home() {
     return (
@@ -18,7 +19,8 @@ export default function Home() {
                     <Button text="Contact-me" className="mt-4 bg-gray-800 text-white rounded-full px-6 py-3 transition-colors hover:bg-gray-700"/>
                 </div>
                 <div className="flex-1 flex justify-center md:justify-end">
-                        {/*Div onde vai ficar a minha imagem*/}
+                        <img src={MyPhoto} alt="Minha fotografia"
+                        className="rounded-full relative bottom-12"/>
                 </div>
             </div>
             <Projects/>
